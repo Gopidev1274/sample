9c84f1844b17f53ae038dd4914353b2418e57ea5
Gopidev1274 <gopigamer.1247@gmail.com>
2019-09-12 10:49:04 +0530

    ex1

---
+++ src/org/prj/Example1.java
@@ -0,0 +1,14 @@
+package org.prj;
+
+import java.util.*;
+
+public class Example1 {
+public static void main(String[] args) {
+	Set<Integer> li=new LinkedHashSet();
+	li.add(1);
+	li.add(2);
+	li.add(3);
+	li.add(4);
+	System.out.println(li);
+}
+}
